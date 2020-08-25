054c301d2b9af474d91968c2b82d23b596de6d79
Willem Jan Faber <willemjan.faber@kb.nl>
2020-08-25 14:28:31 +0200

    Map now looks correct.

--- generate_map.py
+++ generate_map.py
@@ -14,9 +14,6 @@ import scipy.ndimage
 from folium.features import DivIcon
 from pprint import pprint
 
-
-
-
 import statistics
 import json
 import numpy
@@ -33,58 +30,51 @@ data = {}
 data.update(data1)
 data.update(data2)
 
-
 all_temp = []
 seen = {}
 points = []
 
-
-#DataFrame(columns=('lib', 'qty1', 'qty2'))
-
 for item in data:
     try:
         lat = data.get(item).get('lat')
-    except:
+    except Exception:
         continue
+
     lon = data.get(item).get('lon')
     temp = round(data.get(item).get('temp'), 2)
+
     print(lat, lon, temp)
     if lat and lon:
         key = str(lat) + str(lon)
         if not seen.get(key):
             seen[key] = round(temp, 2)
-            points.append([lat, lon, round(temp,2)])
-        #else:
-        #    seen[key] = temp
+            points.append([lat, lon, round(temp, 2)])
 
 df = pd.DataFrame(points, columns=('latitude', 'longitude', 'temperature'))
 for item in seen:
     all_temp.append(seen[item])
 
-print(all_temp, df)
 # Setup
 temp_mean = numpy.mean(all_temp)
-temp_std  = statistics.stdev(all_temp)
-debug     = False
+temp_std = statistics.stdev(all_temp)
+debug = False
 
 # Setup colormap
-
 colors = [
           '#2b83ba',
           '#abdda4',
           '#ffffbf',
-          '#fdae61', 
-          '#d7191c', 
+          '#fdae61',
+          '#d7191c',
           '#ff0000'
 ]
 
-
-vmin   = temp_mean - 2 * temp_std
-vmax   = temp_mean + 2 * temp_std
+vmin = temp_mean - 2 * temp_std
+vmax = temp_mean + 2 * temp_std
 levels = len(colors)
-cm     = branca.colormap.LinearColormap(colors, vmin=vmin, vmax=vmax).to_step(levels)
-
-# Create a dataframe with fake data
+cm = branca.colormap.LinearColormap(colors,
+                                    vmin=vmin,
+                                    vmax=vmax).to_step(levels)
 
 # The original data
 x_orig = np.asarray(df.longitude.tolist())
@@ -92,21 +82,27 @@ y_orig = np.asarray(df.latitude.tolist())
 z_orig = np.asarray(df.temperature.tolist())
 
 # Make a grid
-x_arr          = np.linspace(np.min(x_orig), np.max(x_orig), 1000)
-y_arr          = np.linspace(np.min(y_orig), np.max(y_orig), 1000)
+x_arr = np.linspace(np.min(x_orig), np.max(x_orig), 4000)
+y_arr = np.linspace(np.min(y_orig), np.max(y_orig), 4000)
 x_mesh, y_mesh = np.meshgrid(x_arr, y_arr)
-
 
 # Grid the values
 z_mesh = griddata((x_orig, y_orig), z_orig, (x_mesh, y_mesh), method='linear')
-
 
 # Gaussian filter the grid to make it smoother
 sigma = [4, 4]
 z_mesh = sp.ndimage.filters.gaussian_filter(z_mesh, sigma, mode='constant')
 
 # Create the contour
-contourf = plt.contourf(x_mesh, y_mesh, z_mesh, levels, alpha=0.4, colors=colors, linestyles='None', vmin=vmin, vmax=vmax)
+contourf = plt.contourf(x_mesh,
+                        y_mesh,
+                        z_mesh,
+                        levels,
+                        alpha=0.2,
+                        colors=colors,
+                        linestyles='None',
+                        vmin=vmin,
+                        vmax=vmax)
 
 # Convert matplotlib contourf to geojson
 geojson = geojsoncontour.contourf_to_geojson(
@@ -117,7 +113,9 @@ geojson = geojsoncontour.contourf_to_geojson(
     fill_opacity=0.5)
 
 # Set up the folium plot
-geomap = folium.Map([df.latitude.mean(), df.longitude.mean()], zoom_start=14, tiles="cartodbpositron")
+lat = 51.56899230769231
+lon = 5.0862053846153845
+geomap = folium.Map([lat, lon], zoom_start=14, tiles="cartodbpositron")
 
 # Plot the contour plot on folium
 folium.GeoJson(
@@ -131,17 +129,16 @@ folium.GeoJson(
 
 
 for p in points:
-    lat=p[0]
-    lon=p[1]
-    temp=str(round(seen[str(lat) + str(lon)],2))
-    folium.map.Marker(
-    [lat, lon],
-    icon=DivIcon(
-        icon_size=(150,36),
-        icon_anchor=(0,0),
-        html='<div style="font-size: 24pt">' + temp + '</div>',
-        )
-    ).add_to(geomap)
+    lat = p[0]
+    lon = p[1]
+    temp = str(round(seen[str(lat) + str(lon)], 2))
+    folium.map.Marker([lat, lon],
+                      icon=DivIcon(
+                        icon_size=(150, 36),
+                        icon_anchor=(0, 0),
+                        html='<div style="font-size: 18pt">'
+                             + temp + '</div>',)
+                      ).add_to(geomap)
 
 
 # Add the colormap to the folium map
@@ -149,7 +146,9 @@ cm.caption = 'Temperature'
 geomap.add_child(cm)
 
 # Fullscreen mode
-plugins.Fullscreen(position='topright', force_separate_button=True).add_to(geomap)
+plugins.Fullscreen(position='topright',
+                   force_separate_button=True
+                   ).add_to(geomap)
 
 # Plot the data
 geomap.save(f'out1.html')
